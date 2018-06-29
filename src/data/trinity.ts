1e677cb6483ccccd1fc15da5bef301f2d58a5f26
Martyn Janes <martyn@obany.com>
2018-06-29 15:57:28 +0300

    Initial commit

---
+++ src/data/trinity.ts
@@ -0,0 +1,45 @@
+/**
+ * Class to helper render data for rinity as QR.
+ */
+export class Trinity {
+    /**
+     * Render the QR code data for tinity.
+     * @param address The address trytes.
+     * @param amount The amount for the transaction.
+     * @param tag The tag for the trancation in trytes.
+     * @param message The message for the transaction in trytes.
+     * @returns The data for the rendered qr code.
+     */
+    public static transactionQR(address: string, amount?: number, tag?: string, message?: string): Promise<any> {
+        if (address !== null && address !== undefined && /^[9A-Z]{81}$/.test(address)) {
+            throw new Error("The address does not appear to be in valid trytes format");
+        }
+
+        if (amount !== undefined || !Number.isInteger(amount)) {
+            throw new Error("The amount must be an integer");
+        }
+
+        if (tag !== undefined && tag !== null) {
+            if (tag.length > 27) {
+                throw new Error(`The tag is too long, it should be at most 27 trytes, it is ${tag.length}`);
+            }
+            if (/^[9A-Z]+$/.test(tag)) {
+                throw new Error("The tag does not appear to be in valid trytes format");
+            }
+        }
+
+        const finalData: {
+            address?: string;
+            amount?: number;
+            tag?: string;
+            message?: string;
+        } = {
+            address,
+            amount,
+            message,
+            tag
+        };
+
+        return undefined;
+    }
+}
